a189ecac674307404683af76e3b1209c25112024
Eanur Lihan <lihan.lihu@gmail.com>
2023-11-30 16:12:00 +0600

    I have updated admin role

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g9xsrko.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,14 +29,107 @@ async function run() {
         await client.connect();
 
         const usersCollection = client.db("lifeDb").collection("users");
+        const requestsCollection = client.db("lifeDb").collection("donationRequests");
+
+        // donation related api
+
+        app.get('/donationRequests', async (req, res) => {
+            console.log('inside verify token', req.headers);
+            const result = await requestsCollection.find().toArray();
+            res.send(result);
+        });
+        
+        app.delete('/donationRequests/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await requestsCollection.deleteOne(query);
+            res.send(result);
+          })
+
+        app.post('/donationRequests', async (req, res) => {
+            try {
+              
+              const {
+                requesterName,
+                requesterEmail,
+                recipientName,
+                recipientDistrict,
+                recipientUpazila,
+                hospitalName,
+                fullAddress,
+                donationDate,
+                donationTime,
+                requestMessage,
+                donationStatus,
+              } = req.body;
+        
+              
+              const newDonationRequest = {
+                requesterName,
+                requesterEmail,
+                recipientName,
+                recipientDistrict,
+                recipientUpazila,
+                hospitalName,
+                fullAddress,
+                donationDate,
+                donationTime,
+                requestMessage,
+                donationStatus,
+              };
+        
+              
+              await requestsCollection.insertOne(newDonationRequest);
+        
+              res.json({ success: true, message: 'Donation request created successfully' });
+            } catch (error) {
+              console.error('Error creating donation request:', error);
+              res.status(500).json({ success: false, error: 'Internal server error' });
+            }
+          });
+
 
         //user related api
 
         app.get('/users', async (req, res) => {
-            console.log('inside verify token',req.headers);
+            console.log('inside verify token', req.headers);
             const result = await usersCollection.find().toArray();
             res.send(result);
-          });
+        });
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exist', insertedId: null })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.patch('/users/admin/:id', async (req, res) => {
+          const id = req.params.id;
+          const filter = { _id: new ObjectId(id) };
+          const updatedDoc = {
+            $set: {
+              role: 'admin'
+            }
+          }
+          const result = await usersCollection.updateOne(filter, updatedDoc);
+          res.send(result);
+    
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await usersCollection.deleteOne(query);
+          res.send(result);
+        })
+          
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
