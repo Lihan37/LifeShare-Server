24ce2d81a2344e53241878d611284980beac087d
Eanur Lihan <lihan.lihu@gmail.com>
2023-12-03 02:23:15 +0600

    I have added set active and blocked button on backend and frontend

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -16,128 +22,234 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-        const usersCollection = client.db("lifeDb").collection("users");
-        const requestsCollection = client.db("lifeDb").collection("donationRequests");
-
-        // donation related api
-
-        app.get('/donationRequests', async (req, res) => {
-            console.log('inside verify token', req.headers);
-            const result = await requestsCollection.find().toArray();
-            res.send(result);
-        });
-        
-        app.delete('/donationRequests/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await requestsCollection.deleteOne(query);
-            res.send(result);
-          })
-
-        app.post('/donationRequests', async (req, res) => {
-            try {
-              
-              const {
-                requesterName,
-                requesterEmail,
-                recipientName,
-                recipientDistrict,
-                recipientUpazila,
-                hospitalName,
-                fullAddress,
-                donationDate,
-                donationTime,
-                requestMessage,
-                donationStatus,
-              } = req.body;
-        
-              
-              const newDonationRequest = {
-                requesterName,
-                requesterEmail,
-                recipientName,
-                recipientDistrict,
-                recipientUpazila,
-                hospitalName,
-                fullAddress,
-                donationDate,
-                donationTime,
-                requestMessage,
-                donationStatus,
-              };
-        
-              
-              await requestsCollection.insertOne(newDonationRequest);
-        
-              res.json({ success: true, message: 'Donation request created successfully' });
-            } catch (error) {
-              console.error('Error creating donation request:', error);
-              res.status(500).json({ success: false, error: 'Internal server error' });
-            }
-          });
+    const usersCollection = client.db("lifeDb").collection("users");
+    const requestsCollection = client.db("lifeDb").collection("donationRequests");
 
 
-        //user related api
 
-        app.get('/users', async (req, res) => {
-            console.log('inside verify token', req.headers);
-            const result = await usersCollection.find().toArray();
-            res.send(result);
-        });
-
-
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            const query = { email: user.email }
-            const existingUser = await usersCollection.findOne(query);
-            if (existingUser) {
-                return res.send({ message: 'user already exist', insertedId: null })
-            }
-            const result = await usersCollection.insertOne(user);
-            res.send(result);
-        });
-
-        app.patch('/users/admin/:id', async (req, res) => {
-          const id = req.params.id;
-          const filter = { _id: new ObjectId(id) };
-          const updatedDoc = {
-            $set: {
-              role: 'admin'
-            }
-          }
-          const result = await usersCollection.updateOne(filter, updatedDoc);
-          res.send(result);
-    
-        })
-
-        app.delete('/users/:id', async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: new ObjectId(id) };
-          const result = await usersCollection.deleteOne(query);
-          res.send(result);
-        })
-          
+    // jwt api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
 
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+
+
+    // donation related api
+
+    app.get('/donationRequests', async (req, res) => {
+      console.log('inside verify token', req.headers);
+      const result = await requestsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete('/donationRequests/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await requestsCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.post('/donationRequests', async (req, res) => {
+      try {
+
+        const {
+          requesterName,
+          requesterEmail,
+          recipientName,
+          recipientDistrict,
+          recipientUpazila,
+          hospitalName,
+          fullAddress,
+          donationDate,
+          donationTime,
+          requestMessage,
+          donationStatus,
+        } = req.body;
+
+
+        const newDonationRequest = {
+          requesterName,
+          requesterEmail,
+          recipientName,
+          recipientDistrict,
+          recipientUpazila,
+          hospitalName,
+          fullAddress,
+          donationDate,
+          donationTime,
+          requestMessage,
+          donationStatus,
+        };
+
+
+        await requestsCollection.insertOne(newDonationRequest);
+
+        res.json({ success: true, message: 'Donation request created successfully' });
+      } catch (error) {
+        console.error('Error creating donation request:', error);
+        res.status(500).json({ success: false, error: 'Internal server error' });
+      }
+    });
+
+    // middleware
+    const verifyToken = (req, res, next) => {
+      console.log('inside verified token', req.headers.authorization);
+
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+      })
+
     }
+
+    // verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded?.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    //user related api
+
+    app.get('/users', async (req, res) => {
+      console.log('inside verify token', req.headers);
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+
+      }
+      res.send({ admin });
+    })
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      user.status = 'active';
+
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch('/users/admin/block/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const userId = req.params.id;
+      const filter = { _id: new ObjectId(userId) };
+      const update = { $set: { status: 'blocked' } };
+
+      try {
+        const result = await usersCollection.updateOne(filter, update);
+
+        if (result.modifiedCount > 0) {
+          console.log('User blocked successfully');
+          res.send({ message: 'User blocked successfully' });
+        } else {
+          console.log('User not found');
+          res.status(404).send({ message: 'User not found' });
+        }
+      } catch (error) {
+        console.error('Error blocking user:', error);
+        res.status(500).send({ message: 'Internal server error' });
+      }
+    });
+
+    app.patch('/users/admin/unblock/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const userId = req.params.id;
+      const filter = { _id: new ObjectId(userId) };
+      const update = { $set: { status: 'active' } };
+      try {
+        const result = await usersCollection.updateOne(filter, update);
+
+        if (result.modifiedCount > 0) {
+          res.send({ message: 'User unblocked successfully' });
+        } else {
+          res.status(404).send({ message: 'User not found' });
+        }
+      } catch (error) {
+        console.error('Error unblocking user:', error);
+        res.status(500).send({ message: 'Internal server error' });
+      }
+    });
+
+
+
+
+
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+
+    })
+
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
@@ -145,9 +257,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('LifeShare is here')
+  res.send('LifeShare is here')
 })
 
 app.listen(port, () => {
-    console.log(`LifeShare is running on port ${port}`);
+  console.log(`LifeShare is running on port ${port}`);
 })
